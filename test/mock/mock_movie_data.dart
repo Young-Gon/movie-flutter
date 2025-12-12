616ed318cd0e9b39a90f919d6b32a32f4d5c2eb6
이영곤 <guest110@skylabs.io>
2025-12-12 18:03:46 +0900

    테스트 코드 추가

---
+++ test/mock/mock_movie_data.dart
@@ -0,0 +1,17 @@
+// 테스트를 위한 MovieModel Mock 데이터
+final mockMovieJson = {
+  "adult": false,
+  "backdrop_path": "/wNAU34sHRc1r1I32yI2dI7l7k2s.jpg",
+  "genre_ids": [28, 12, 878],
+  "id": 786892,
+  "original_language": "en",
+  "original_title": "Furiosa: A Mad Max Saga",
+  "overview": "세상이 멸망하고 45년, 인류는 황폐해진 세상에 익숙해졌다. 녹색의 땅을 찾아 헤매던 그녀, '퓨리오사'는 바이커 군단의 폭군 '디멘투스'의 손에 모든 것을 잃고 만다. 가족도, 행복도, 희망도 없이 세상에 홀로 내던져진 '퓨리오사'는 복수를 위해 자신의 모든 것을 걸기로 결심하는데... 매드맥스 없는 매드맥스, 분노의 도로 그 이전, 퓨리오사의 과거가 깨어난다!",
+  "popularity": 3289.43,
+  "poster_path": "/9p36wYcstC9B2iS2m3a223O5y19.jpg",
+  "release_date": "2024-05-22",
+  "title": "퓨리오사: 매드맥스 사가",
+  "video": false,
+  "vote_average": 7.7,
+  "vote_count": 1419
+};
